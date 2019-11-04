5937f0c8938624ca4c56ae7363decda3baad439a
HgPi4 <hg@example.se>
2019-11-04 20:29:19 +0100

    ctc wd update

--- ctc_watchdog.py
+++ ctc_watchdog.py
@@ -2,10 +2,9 @@
 
 import time
 import schedule
-import subprocess
-import telnetlib
 import json
 import os
+import subprocess
 
 import RPi.GPIO as GPIO
 from sheetLog.SheetItf import SheetItf
@@ -24,53 +23,80 @@ class Watchdog:
         GPIO.setwarnings(True)
 
         GPIO.setup(self.cfg['RELAY_ENABLE'], GPIO.OUT)
-        GPIO.output(self.cfg['RELAY_ENABLE'], GPIO.LOW)
+        GPIO.output(self.cfg['RELAY_ENABLE'], GPIO.HIGH)
 
         # logger
         self.sheet = SheetItf(config.GOOGLE)
 
-        self.log("Watchdog initiated")
-        self.tests = 1
+        self.log(["Watchdog initiated"])
+        self.tests = 0
+        self.fails = 0
+        self.failCnt = 0
+        self.reboots = 0
+        self.enabled = 1
 
     def check(self):
-        FNULL = open(os.devnull, 'w')
-        error = subprocess.call(["ping", "-c1", self.cfg['localhost']], stdout=FNULL)
 
-        if error == 0:
-            # success
-            print("ping success!")
-            if self.tests == 0:
-                self.log("Ping ok")
-                self.tests += 1
-        elif error == 1:
-            print("local host unreachable")
-            if self.tests > 0:
-                self.tests = 0
-                self.log("Ping failed")
-            self.ctc_reboot()
-        elif error == 2:
-            print("local network unreachable")
-        else:
-            print("unknown local error: {}".format(error))
+        self.enabledCheck()
+
+        if self.enabled > 0:
+            FNULL = open(os.devnull, 'w')
+            error = subprocess.call(["ping", "-c1", self.cfg['localhost']], stdout=FNULL)
+            self.tests += 1
+
+            if error == 0:
+                # success
+                print("ping success!")
+                if self.failCnt > 0:
+                    self.log(["Ping ok"])
+                self.failCnt = 0
+            elif error == 1:
+                print("host unreachable")
+                self.fails +=1
+                self.failCnt +=1
+                if self.failCnt == 0:
+                    self.tests = 0
+                    self.log(["Ping failed"])
+                elif self.failCnt >= 6:
+                    self.ctc_reboot()
+                    self.failCnt = 0 
+            elif error == 2:
+                print("local network unreachable")
+            else:
+                print("unknown local error: {}".format(error))
+
+    def enabledCheck(self):
+        enabled = int(self.sheet.getCell("setup","B1"))
+        if self.enabled != enabled:
+            self.log(["Watchdog enabled: %d" % enabled])
+        self.enabled = enabled
 
     def ctc_reboot(self):
         print("Rebooting CTC internet module...")
-        GPIO.output(self.cfg['RELAY_ENABLE'], GPIO.HIGH)
-        time.sleep(5)
         GPIO.output(self.cfg['RELAY_ENABLE'], GPIO.LOW)
-        print("Done")
+        time.sleep(5)
+        GPIO.output(self.cfg['RELAY_ENABLE'], GPIO.HIGH)
+        print("Reboot done")
+
+        self.reboots += 1
+        self.log(["Reboot"])
 
     def log(self, txt):
         self.sheet.addToRow([str(time.strftime("%d/%m/%Y")), str(time.strftime("%H:%M:%S"))])
-        self.sheet.addToRow([txt])
+        self.sheet.addToRow(txt)
         print("pushing:", self.sheet.nextRow)
         self.sheet.pushRow()
+
+    def logStat(self):
+        self.log([self.tests, self.reboots, self.fails])
 
 # ------------------------------------------------------
 wd = Watchdog()
 
 # Schedule every..
 schedule.every(0.5).minutes.do(wd.check)
+#schedule.every(10).minutes.do(wd.logStat)
+schedule.every().hour.at(":00").do(wd.logStat)
 
 
 # ------------------------------------------------------
