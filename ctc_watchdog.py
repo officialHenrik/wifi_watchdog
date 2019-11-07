202dc3227fc5c3631df114874ef44c4c5724d6c5
HgPi4 <hg@example.se>
2019-11-07 22:14:17 +0100

    correct check interval

--- ctc_watchdog.py
+++ ctc_watchdog.py
@@ -88,14 +88,14 @@ class Watchdog:
         self.sheet.pushRow()
 
     def logStat(self):
+        self.
         self.log([self.tests, self.reboots, self.fails])
 
 # ------------------------------------------------------
 wd = Watchdog()
 
 # Schedule every..
-schedule.every(0.5).minutes.do(wd.check)
-#schedule.every(10).minutes.do(wd.logStat)
+schedule.every(10).minutes.do(wd.logStat)
 schedule.every().hour.at(":00").do(wd.logStat)
 
 
